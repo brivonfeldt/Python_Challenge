03bb23d56c5c8c1b654728b333c3fa908fa4d09d
Bri VonFeldt <briv1224@gmai.com>
2019-03-13 15:43:28 -0600

    prints the  str of row and the len of each row

--- PyBank/main_pybank.py
+++ PyBank/main_pybank.py
@@ -5,11 +5,17 @@ csvpath = os.path.join(".", "PyBank.csv")
 
 with open(csvpath, newline= "") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
-    
-    print(csvreader)
+
+    #print(csvreader)
 
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+    #print(f"CSV Header: {csv_header}")
 
+    date_column = ([0, 0])
+
+    number_of_months = len(date_column)
+                
     for row in csvreader:
         print(row)
+        print(number_of_months)
+    
