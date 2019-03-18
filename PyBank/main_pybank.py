6dd958e1462c090dcafa089834b7a20b8c601618
Bri VonFeldt <briv1224@gmai.com>
2019-03-17 18:21:10 -0600

    totals months

--- PyBank/main_pybank.py
+++ PyBank/main_pybank.py
@@ -1,14 +1,34 @@
 import os
 import csv
 
-csvpath = os.path.join(".", "PyBank.csv")
+csvpath = os.path.join('.', 'PyBank.csv')
 
-with open(csvpath, newline= "") as csvfile:
+with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     print(csvreader)
     csv_header = next(csvreader)
-    #print(f"CSV Header: {csv_header}")
-    date_column = list(csvreader)
-    number_of_months = len(date_column)         
-    print(f"The total of months are : {number_of_months}") 
+    print(f'CSV Header: {csv_header}')
+    reader_as_list = list(csvreader)
+    number_of_months = len(reader_as_list)         
+    print(f'The total of months are : {number_of_months}') 
+    print('---')
     
+    net_total_pl = 0 
+
+    for row in reader_as_list :
+        date_column = (row[0])
+        profit_and_loss_column = (row[1])
+        print(date_column+' '+profit_and_loss_column)
+        #print(profit_and_loss_column)
+
+    def finding_sum() :
+        profit_and_loss_total = 0
+        for row in reader_as_list :
+            profit_and_loss_total += profit_and_loss_column
+            print(profit_and_loss_total)
+           
+ #   finding_sum()
+
+
+
+
