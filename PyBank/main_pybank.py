4e8a1e337963204682a4e8146c0e603594d8790d
Bri VonFeldt <briv1224@gmai.com>
2019-03-17 19:36:40 -0600

    getting average of changes

--- PyBank/main_pybank.py
+++ PyBank/main_pybank.py
@@ -14,6 +14,11 @@ with open(csvpath, newline='') as csvfile:
     print('---')
     
     net_total_pl = 0 
+    average_of_pl = 0
+    total_of_differences_between_each_rows = 0
+    current_row_value = 0
+    last_row_value = 0
+    
 
     for row in reader_as_list :
         date_column = (row[0])
@@ -21,8 +26,20 @@ with open(csvpath, newline='') as csvfile:
         print(date_column+' '+profit_and_loss_column)
         #print(profit_and_loss_column)
         net_total_pl += int(profit_and_loss_column)
-    
+        # get the difference between this row and the last row
+        current_row_value = profit_and_loss_column
+        differences_between_these_rows = int(current_row_value) - int(last_row_value)
+        #print(differences_between_these_rows )
+        total_of_differences_between_each_rows += differences_between_these_rows
+        last_row_value = current_row_value
+                
     print(f'Net total profit and loss is : {net_total_pl}')
+    print(f'total of all differences : {total_of_differences_between_each_rows}')
+
+    #calc avg of differences
+    average_of_pl = total_of_differences_between_each_rows / number_of_months
+
+    print(average_of_pl)
 
 
     def finding_sum() :
