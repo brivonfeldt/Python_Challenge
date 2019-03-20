8ef9036c2684b0292e7fbd67d9d006efdf23f1b3
Bri VonFeldt <briv1224@gmai.com>
2019-03-19 21:55:33 -0600

    this does nothing.. want to save thoughts

--- PyBank/main_pybank.py
+++ PyBank/main_pybank.py
@@ -16,22 +16,23 @@ with open(csvpath, newline='') as csvfile:
     net_total_pl = 0 
     average_of_pl = 0
     total_of_differences_between_each_rows = 0
-    current_row_value = 0
-    last_row_value = 0
+    next_row_value = 0
     
-
     for row in reader_as_list :
         date_column = (row[0])
         profit_and_loss_column = (row[1])
-        print(date_column+' '+profit_and_loss_column)
+        #print(f'{date_column} {profit_and_loss_column}')
         #print(profit_and_loss_column)
         net_total_pl += int(profit_and_loss_column)
         # get the difference between this row and the last row
-        current_row_value = profit_and_loss_column
-        differences_between_these_rows = int(current_row_value) - int(last_row_value)
-        #print(differences_between_these_rows )
+        differences_between_these_rows = int(profit_and_loss_column) - int(next_row_value)
+        difference_list = zip(str(differences_between_these_rows))
+
+        print(difference_list)
+        next_row_value = profit_and_loss_column
+
         total_of_differences_between_each_rows += differences_between_these_rows
-        last_row_value = current_row_value
+        #last_row_value = current_row_value
                 
     print(f'Net total profit and loss is : {net_total_pl}')
     print(f'total of all differences : {total_of_differences_between_each_rows}')
@@ -40,16 +41,4 @@ with open(csvpath, newline='') as csvfile:
     average_of_pl = total_of_differences_between_each_rows / number_of_months
 
     print(average_of_pl)
-
-
-    def finding_sum() :
-        profit_and_loss_total = 0
-        for row in reader_as_list :
-            profit_and_loss_total += profit_and_loss_column
-            print(profit_and_loss_total)
-           
- #   finding_sum()
-
-
-
 
